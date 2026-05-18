e6c74d6c0bc945981f26900c356b979d77cab0f8
New Username <newemail@example.com>
2026-05-18 18:42:57 +0530

    Add /socket.io proxy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,8 +22,12 @@ export default defineConfig({
       '/files': {
         target: 'http://127.0.0.1:2000',
         changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://127.0.0.1:2000',
+        changeOrigin: true,
+        ws: true
       }
-      
     }
   }
 });
